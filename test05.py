e5b8e490a15e0f7a09ab864bc9dc5685205ab562
crane0731 <dlwnsgkr8318@gmail.com>
2024-10-02 15:53:09 +0900

    .

--- test05.py
+++ test05.py
@@ -9,19 +9,10 @@ index=0
 
 for i in range(len(text)-len(answer)+1):
 
-    print(text[index : len(answer) + index])
-    print(answer)
-
     if text[index : len(answer)+index]==answer:
         count+=1
         index+=len(answer)
-        print("index="+str(index))
-
-        print("일치")
-        print()
     else:
         index+=1
-        print("불일치")
-        print()
 
 print(count)
